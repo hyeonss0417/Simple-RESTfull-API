9b5df79a0bba1294b1ad3c7b7aa3ee77708f8e7c
전현성 <skyoun97@gmail.com>
2020-06-17 02:22:34 +0900

    [Finish] - All CRUD test

---
+++ models/subscriber.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const subscriberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  subscribedToChannel: {
+    type: String,
+    required: true,
+  },
+  subscribeDate: {
+    type: Date,
+    required: true,
+    default: () => Date.now(),
+  },
+});
+
+const Subscriber = mongoose.model("Subscriber", subscriberSchema);
+export default Subscriber;
--- src/routes/subscribers.js
+++ src/routes/subscribers.js
@@ -1,22 +1,78 @@
 import express from "express";
+import Subscriber from "../../models/subscriber";
 
 const subscribersRouter = express.Router();
 
 // Getting All
-subscribersRouter.get("/", (req, res) => {});
+subscribersRouter.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // Getting One
-subscribersRouter.get("/:id", (req, res) => {});
+subscribersRouter.get("/:id", getSubscriber, (req, res) => {
+  res.json(res.subscriber);
+});
 
 // Creating One
-subscribersRouter.post("/", (req, res) => {});
+subscribersRouter.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // Updating One
-subscribersRouter.patch("/:id", (req, res) => {});
+subscribersRouter.patch("/:id", getSubscriber, async (req, res) => {
+  if (req.body.name !== null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // patch (update only things given from client) vs put (update all things at once)
 
 // Deleting One
-subscribersRouter.delete("/:id", (req, res) => {});
+subscribersRouter.delete("/:id", getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({ message: "Deleted Subscriber" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber === null) {
+      return res.status(404).json({ message: "Cannot find subscriber" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+}
 
 export default subscribersRouter;
